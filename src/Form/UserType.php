aa79155d4d5e32003e0fc849f1aacee8356770d4
morganNwt <metodik60@hotmail.fr>
2024-06-27 15:52:28 +0200

    feat(update): add update users

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -64,12 +65,29 @@ class UserType extends AbstractType
                 ],
                 'mapped'=> 'false',
             ]);
+
+            if($options['isAdmin']) {
+                $builder    
+                        ->remove('password')
+                        ->add('roles', ChoiceType::class, [
+                            'label' => 'Rôle',
+                            'choices' => [
+                                'Utilisateur' => 'ROLE_USER',
+                                'éditeur' =>'ROLE_EDITOR',
+                                'Administrateur' => 'ROLE_ADMIN',
+                            ],
+                            'expanded' => true,
+                            'multiple' => true,
+                        ]);
+
+            }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'isAdmin' => false,
         ]);
     }
 }
